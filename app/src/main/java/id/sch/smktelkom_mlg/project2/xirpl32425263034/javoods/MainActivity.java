fc37be13f2df7669aac1b26e464f693158ad61e0
User <septinarika90@gmail.com>
2017-04-07 22:51:35 +0700

    home design

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl32425263034/javoods/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl32425263034/javoods/MainActivity.java
@@ -2,6 +2,8 @@ package id.sch.smktelkom_mlg.project2.xirpl32425263034.javoods;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -11,13 +13,49 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
     ImageView ivMakan, ivMinum, ivCemilan;
 
+    public static int calculateInSampleSize(BitmapFactory.Options options,
+                                            int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and
+            // keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(String strPath, int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(strPath, options);
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth,
+                reqHeight);
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(strPath, options);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ivMakan = (ImageView) findViewById(R.id.imageView);
-        ivMinum = (ImageView) findViewById(R.id.imageView2);
-        ivCemilan = (ImageView) findViewById(R.id.imageView3);
+        ivMakan = (ImageView) findViewById(R.id.ivMakanan);
+        ivMinum = (ImageView) findViewById(R.id.ivMinuman);
+        ivCemilan = (ImageView) findViewById(R.id.ivCemilan);
 
         ivMakan.setOnClickListener(new View.OnClickListener() {
             @Override
